7b66dbbcbcec9f5383f2a0eba3815a1abbb2015e
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-04-07 16:40:52 +0400

    Web Service Start

---
+++ rsvbot/BotService.py
@@ -0,0 +1,55 @@
+import random
+import json
+
+import torch
+
+from botModel import NeuralNet
+from botUtils import wdBag, token
+from fastapi import FastAPI
+
+app = FastAPI()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+@app.get("/bot/{answer}")
+def get_botResponse(answer: str):
+    with open('botContent/data/skilld.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    FILE = "botContent/skills/textskill.pth"
+    data = torch.load(FILE)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+
+    sentence = answer
+    sentence = token(sentence)
+    X = wdBag(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent['responses'])
+    else:
+        response = "Извините, я не понял запрос..."
+
+    return {"botResponse": response}
+
+
