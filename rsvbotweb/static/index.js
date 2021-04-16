2f312e0c8f7160ddba5aa1a728d62a740dc4cb5d
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-04-16 15:32:48 +0400

    New Skills and Demo Update

--- rsvbotweb/static/index.js
+++ rsvbotweb/static/index.js
@@ -51,4 +51,27 @@ $(document).ready(function () {
 
         LastMessage();
     });
+    $('#rsv-bot > main .bot-content p ul.accept li, #rsv-bot > main .bot-content p ul.skillslist li').click(function (e,t) { 
+        e.preventDefault();
+        
+        let userQuery = $(this).attr('data-query'),
+            sourceBut = $(this).text(),
+            chatContent = $('#rsv-bot > main .bot-content'),
+            answer = $('p#query'),
+            response = $('p#response');
+
+        $(`<p id="query">${sourceBut}</p>`).appendTo(chatContent);
+        
+        $.ajax({
+            type: "GET",
+            url: botService + encodeURIComponent(userQuery),
+            success: function (response) {
+                const responseJson = JSON.parse(JSON.stringify(response));
+
+                $(`<p id="response">${responseJson.botResponse}</p>`).appendTo(chatContent);
+            }
+        });
+
+        LastMessage();
+    });
 });
